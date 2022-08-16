0f4c6bf79625afa56fd430905e748c369598bc6a
Hippie Hacker <hh@cncf.io>
2022-08-16 13:47:54 -0600

    Update inputs to not use '-'
    Co-Authored-By: RobKielty <RobKielty@users.noreply.github.com>

--- prow/cmd/hook/kodata/pga-entrypoint.sh
+++ prow/cmd/hook/kodata/pga-entrypoint.sh
@@ -13,20 +13,20 @@ PLUGIN_CONFIGFILE=$HOME/plugins.yaml
 HMAC_FILE=$HOME/hmac
 GITHUB_TOKENFILE=$HOME/github_token
 
-if [ "${INPUT_HOOK-CONFIG}" != "" ]; then
-    echo "${INPUT_HOOK-CONFIG}" > "${PROW_CONFIGFILE}"
+if [ "${INPUT_PROW_CONFIG}" != "" ]; then
+    echo "${INPUT_PROW_CONFIG}" > "${PROW_CONFIGFILE}"
 else
     cp "/var/run/ko/config.yaml" "${PROW_CONFIGFILE}"
 fi
 
-if [ "${INPUT_PLUGIN-CONFIG}" != "" ]; then
-    echo "${INPUT_PLUGIN-CONFIG}" > "${PLUGIN_CONFIGFILE}"
+if [ "${INPUT_PLUGIN_CONFIG}" != "" ]; then
+    echo "${INPUT_PLUGIN_CONFIG}" > "${PLUGIN_CONFIGFILE}"
 else
     cp "/var/run/ko/plugins.yaml" "${PLUGIN_CONFIGFILE}"
 fi
 
-if [ "${HMAC}" != "" ]; then
-    echo "${HMAC}" > "${HMAC_FILE}"
+if [ "${INPUT_HMAC}" != "" ]; then
+    echo "${INPUT_HMAC}" > "${HMAC_FILE}"
 else
     cp "/var/run/ko/hmac" "${HMAC_FILE}"
 fi
