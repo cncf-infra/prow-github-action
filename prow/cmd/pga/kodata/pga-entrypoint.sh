dd1ff4806f832f14da4b607f9916f962762da882
Robert Kielty <rob@ii.coop>
2022-09-06 17:30:35 +0100

    adds unsplash key and tidyup

--- prow/cmd/pga/kodata/pga-entrypoint.sh
+++ prow/cmd/pga/kodata/pga-entrypoint.sh
@@ -13,6 +13,9 @@ PLUGIN_CONFIGFILE=$HOME/plugins.yaml
 HMAC_FILE=$HOME/hmac
 GITHUB_TOKENFILE=$HOME/github_token
 
+# Goose plugin https://github.com/kubernetes/test-infra/blob/master/prow/plugins/goose/goose.go#L58
+UNSPLASH_KEYFILE=/etc/unsplash-api/honk.txt
+
 if [ "${INPUT_PROW_CONFIG}" != "" ]; then
     echo "${INPUT_PROW_CONFIG}" > "${PROW_CONFIGFILE}"
 else
@@ -32,13 +35,17 @@ else
 fi
 
 echo "${GITHUB_TOKEN}" > "${GITHUB_TOKENFILE}"
+echo "${UNSPLASH_ACCESS_KEY}" > "${UNSPLASH_KEYFILE}"
+
 
 set +e
 apk add git
 
-/ko-app/pga \
+/ko-app/hook \
     --config-path "${PROW_CONFIGFILE}" \
     --plugin-config "${PLUGIN_CONFIGFILE}" \
     --hmac-secret-file "${HMAC_FILE}" \
     --github-token-path "${GITHUB_TOKENFILE}" \
     --dry-run=false
+
+sleep 9999
